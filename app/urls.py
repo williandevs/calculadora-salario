b6a2ca1ae5c3b1d69bdc9f56116484a794f38784
williandevs <98467171+williandevs@users.noreply.github.com>
2024-09-05 12:25:57 -0300

    Atualizar configuração para arquivos estáticos

--- app/urls.py
+++ app/urls.py
@@ -5,9 +5,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('calculadora.urls')),  # Inclui as URLs do aplicativo calculadora
+    path('', include('calculadora.urls'))
 ]
 
-# Adicione esta linha apenas se estiver no ambiente de desenvolvimento
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
