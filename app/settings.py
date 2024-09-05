7a686c749577c241ab58374961401ad6c90beb06
williandevs <98467171+williandevs@users.noreply.github.com>
2024-09-05 11:34:52 -0300

    json5

--- app/settings.py
+++ app/settings.py
@@ -65,12 +65,19 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
     }
 }
+
 
 
 # Password validation
